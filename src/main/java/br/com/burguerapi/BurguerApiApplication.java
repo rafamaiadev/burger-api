09c8fc8159a71496ea3ee39c87a8cb8e96eb8e29
rafamaiadev <119112502+rafamaiadev@users.noreply.github.com>
2023-11-13 09:55:19 -0300

    criado

---
+++ src/main/java/br/com/burguerapi/BurguerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.burguerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurguerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurguerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/burguerapi/BurguerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.burguerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurguerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
